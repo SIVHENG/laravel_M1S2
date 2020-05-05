207ba4759523c1f39f63ca7e3a7ed537a918ee91
sengahk <58158957+sengahk@users.noreply.github.com>
2020-05-05 20:29:15 +0700

    this is my homework

---
+++ resources/views/include/class.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script crossorigin="anonymous" src="https://kit.fontawesome.com/69a884fb35.js"></script>
+
+    <!-- JQuery -->
+    <script src="lib/jquery.js"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" media="all">
+</head>
---
+++ resources/views/index.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center my-3">
+
+        <h1>This is Homepage!</h1>
+        <p>
+            This is a template for a simple marketing or informational
+            website. It includes a large callout called the hero unit
+            and three supporting pieces of content. Use it as a
+            starting point to create something more unique.
+        </p>
+      
+    </div>
+@endsection
